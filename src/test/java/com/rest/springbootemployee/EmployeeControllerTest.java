47eec92432e41601ce35713ce171ae55f2edced6
Laughinggg <953651397@qq.com>
2022-07-27 11:35:59 +0800

    [Kendirck]test: should_create_new_employee_when_perform_post_given_new_employee

--- src/test/java/com/rest/springbootemployee/EmployeeControllerTest.java
+++ src/test/java/com/rest/springbootemployee/EmployeeControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -20,21 +21,41 @@ public class EmployeeControllerTest {
     private MockMvc mockMvc;
 
     @BeforeEach
-    public void cleanDB(){
+    public void cleanDB() {
         employeeRepository.cleanAll();
     }
 
     @Test
-    public void should_return_employees_when_getAllEmployees() throws Exception{
+    public void should_return_employees_when_getAllEmployees() throws Exception {
         employeeRepository.addAEmployee(new Employee(1, "Kendrick", 22, "male", 20000));
 
         mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.*",hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.*", hasSize(1)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").isNumber())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Kendrick"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].gender").value("male"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].salary").value(20000))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value(22));
+    }
+
+    @Test
+    public void should_create_new_employee_when_perform_post_given_new_employee() throws Exception {
+        String newEmployee="{\n" +
+                "                \"id\": 2,\n" +
+                "                \"name\": \"Kendraxxxxick\",\n" +
+                "                \"age\": 12,\n" +
+                "                \"gender\": \"male\",\n" +
+                "                \"salary\": 30000\n" +
+                "            }";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/employees")
+                .contentType(MediaType.APPLICATION_JSON).content(newEmployee))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Kendraxxxxick"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.gender").value("male"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(12))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.salary").value(30000));
+
     }
 }
